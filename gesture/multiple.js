9a116d32fc38681041473aecb49953209d960670
tomorrowcho <yoonucho@nolgong.com>
2022-10-22 01:29:33 +0900

    edit testing!

--- gesture/multiple.js
+++ gesture/multiple.js
@@ -21,47 +21,17 @@ AFRAME.registerComponent("markerhandler", {
 	},
 });
 
-AFRAME.registerComponent("clicklisteneranimated", {
-	init: function () {
-		this.el.addEventListener("click", () => {
-			removeEverything();
-			document.getElementById("redShoeEntity").setAttribute("visible", "true");
-			document.getElementById("redShoeEntity").setAttribute("gesture-handler", "enabled: true");
-		});
-	},
-});
-
-AFRAME.registerComponent("clicklistenerred", {
-	init: function () {
-		this.el.addEventListener("click", () => {
-			removeEverything();
-			document.getElementById("blueShoeEntity").setAttribute("visible", "true");
-			document.getElementById("blueShoeEntity").setAttribute("gesture-handler", "enabled: true");
-		});
-	},
-});
-
-AFRAME.registerComponent("clicklistenerblue", {
-	init: function () {
-		this.el.addEventListener("click", () => {
-			removeEverything();
-			document.getElementById("blackShoeEntity").setAttribute("visible", "true");
-			document.getElementById("blackShoeEntity").setAttribute("gesture-handler", "enabled: true");
-		});
-	},
-});
-
 AFRAME.registerComponent("clicklistenerblack", {
 	init: function () {
 		this.el.addEventListener("click", () => {
 			removeEverything();
-			document.getElementById("animatedShoeEntity").setAttribute("visible", "true");
-			document.getElementById("animatedShoeEntity").setAttribute("gesture-handler", "enabled: true");
+			document.getElementById("bowser-model2").setAttribute("visible", "true");
+			document.getElementById("bowser-model2").setAttribute("gesture-handler", "enabled: true");
 		});
 	},
 });
 
 function removeEverything() {
-	document.getElementById("animatedBoxEntity").setAttribute("visible", "false");
-	document.getElementById("animatedBoxEntity").setAttribute("gesture-handler", "enabled: false");
+	document.getElementById("bowser-model").setAttribute("visible", "false");
+	document.getElementById("bowser-model").setAttribute("gesture-handler", "enabled: false");
 }
