f016b157e10b3cfd71b3a344db061e24fa839c10
tomorrowcho <yoonucho@nolgong.com>
2022-10-22 01:13:08 +0900

    testing..

--- gesture/multiple.js
+++ gesture/multiple.js
@@ -8,16 +8,13 @@ AFRAME.registerComponent("markerhandler", {
 	init: function () {
 		this.el.sceneEl.addEventListener("markerFound", () => {
 			if (loadOnce) {
-				document
-					.getElementById("animatedBoxEntity")
-					.setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
-				document.getElementById("animatedBoxEntity").setAttribute("visible", "true");
-				document.getElementById("animatedBoxEntity").setAttribute("gesture-handler", "enabled: true");
-				document
-					.getElementById("animatedShoeEntity")
-					.setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
-				document.getElementById("animatedShoeEntity").setAttribute("visible", "true");
-				document.getElementById("animatedShoeEntity").setAttribute("gesture-handler", "enabled: true");
+				document.getElementById("bowser-model").setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
+				document.getElementById("bowser-model").setAttribute("visible", "true");
+				document.getElementById("bowser-model").setAttribute("gesture-handler", "enabled: true");
+
+				document.getElementById("bowser-model2").setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
+				document.getElementById("bowser-model2").setAttribute("visible", "true");
+				document.getElementById("bowser-model2").setAttribute("gesture-handler", "enabled: true");
 				loadOnce = false;
 			}
 		});
