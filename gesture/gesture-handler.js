1b96ee7fcc8cbccc66626281f8c07ec44a0503f4
yoonucho <yoonucho@nolgong.com>
2022-10-24 16:28:21 +0900

    edit

--- gesture/gesture-handler.js
+++ gesture/gesture-handler.js
@@ -58,7 +58,7 @@ AFRAME.registerComponent("gesture-handler", {
 
 	handleScale: function (event) {
 		if (this.isVisible) {
-			console.log(this);
+			console.log(this.data);
 			this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread;
 
 			this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.minScale), this.data.maxScale);
