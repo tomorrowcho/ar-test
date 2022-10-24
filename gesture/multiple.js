f298d79c76532ad2009dee95bd69a2a2c161017c
yoonucho <yoonucho@nolgong.com>
2022-10-24 13:59:15 +0900

    edit js test

--- gesture/multiple.js
+++ gesture/multiple.js
@@ -8,11 +8,9 @@ AFRAME.registerComponent("markerhandler", {
 	init: function () {
 		this.el.sceneEl.addEventListener("markerFound", () => {
 			if (loadOnce) {
-				document.getElementById("bowser-model").setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
 				document.getElementById("bowser-model").setAttribute("visible", "true");
 				document.getElementById("bowser-model").setAttribute("gesture-handler", "enabled: true");
 
-				document.getElementById("bowser-model2").setAttribute("animation-mixer", "clampWhenFinished: true; loop: once;");
 				document.getElementById("bowser-model2").setAttribute("visible", "true");
 				document.getElementById("bowser-model2").setAttribute("gesture-handler", "enabled: true");
 				loadOnce = false;
